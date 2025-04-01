336e1d30bb5e81c46c64507123b318c508f0edb1
saravanankuppusamy <37090060+saravanankuppusamy@users.noreply.github.com>
2025-04-01 17:30:58 -0400

    Add files via upload

---
+++ legacy-react-labs/lab1/first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>First App</h1>
+      <p>Some text...</p>
+    </div>
+  );
+}
+
+export default App;
